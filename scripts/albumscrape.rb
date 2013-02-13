513d684a745a990bbe30d07d565f46b25e22c80c
Alex Skorulis <alex.skorulis@thomsonreuters.com>
2013-02-13 17:37:42 +0000

    More work on the scaler.

--- scripts/albumscrape.rb
+++ scripts/albumscrape.rb
@@ -7,6 +7,7 @@ require 'json'
 @token = "AAACEdEose0cBAD6pMTo7Mqh1dOSr1ygMMtRXmSyVWOAfzY5X6OAcZAL7XignniSzPXP9eJo5126ltRIqvhUCKZAHEQ0ay255uKGpg46gZDZD";
 @album = "10151283325498745"
 @firstUrl = "https://graph.facebook.com/"+@album+"?fields=photos.limit(10)&access_token="+@token;
+@allBeers = [];
 
 def downloadChunk(url)
 	puts "downloading " + url
@@ -14,12 +15,27 @@ def downloadChunk(url)
 	data = clnt.get_content(url)
 	result = JSON.parse(data)
 	photos = result["photos"]["data"];
-	puts "Photos " + photos.count.to_s()
+	
+	
 	
 	photos.each{|value|
-		puts value["name"];
+		hash = Hash[];
+		hash["name"] = value["name"]
+		hash["img"] = value["source"];
+		@allBeers.push(hash);
 	}
 	
 end
 
+def dumpJSToFile(filename)
+	@allBeers.each{|beer|
+		puts "this.allData.push(App.Beer.create({"
+		puts "\tname:'"+beer["name"]+"',"
+		puts "}));\n"
+	}	
+
+end
+
 downloadChunk(@firstUrl);
+
+dumpJSToFile("beer.js");
